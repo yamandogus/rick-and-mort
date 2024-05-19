50ea719531120d5f5ca82cc410d7cb5caf483ba3
Doğuş Yaman <152088713+yamandogus@users.noreply.github.com>
2024-05-19 17:12:17 +0300

    about change

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,6 +1,264 @@
-
 export default function About() {
   return (
-    <div>about</div>
-  )
+    <div>
+      <h1>Rick and Morty</h1>
+      <p>
+        <i>
+          <b data-sider-select-id="4f530ba0-cfa3-448c-9e4e-169a1df27e29">
+            Rick and Morty
+          </b>
+        </i>{" "}
+        is an American{" "}
+        <a href="/wiki/Adult_animation" title="Adult animation">
+          adult animated
+        </a>{" "}
+        <a href="/wiki/Science_fiction" title="Science fiction">
+          science fiction
+        </a>{" "}
+        <a href="/wiki/Animated_sitcom" title="Animated sitcom">
+          sitcom
+        </a>{" "}
+        created by{" "}
+        <a href="/wiki/Justin_Roiland" title="Justin Roiland">
+          Justin Roiland
+        </a>{" "}
+        and{" "}
+        <a href="/wiki/Dan_Harmon" title="Dan Harmon">
+          Dan Harmon
+        </a>{" "}
+        for{" "}
+        <a href="/wiki/Cartoon_Network" title="Cartoon Network">
+          Cartoon Network
+        </a>
+        's nighttime programming block{" "}
+        <a href="/wiki/Adult_Swim" title="Adult Swim">
+          Adult Swim
+        </a>
+        . The series follows the misadventures of{" "}
+        <a href="/wiki/Rick_Sanchez" title="Rick Sanchez">
+          Rick Sanchez
+        </a>
+        , a cynical{" "}
+        <a href="/wiki/Mad_scientist" title="Mad scientist">
+          mad scientist
+        </a>
+        , and his good-hearted but fretful grandson{" "}
+        <a href="/wiki/Morty_Smith" title="Morty Smith">
+          Morty Smith
+        </a>
+        , who split their time between domestic life and{" "}
+        <a
+          href="/wiki/Interdimensional_hypothesis"
+          className="mw-redirect"
+          title="Interdimensional hypothesis"
+        >
+          interdimensional
+        </a>{" "}
+        adventures that take place across an infinite number of realities, often
+        traveling to other planets and dimensions through portals and on Rick's{" "}
+        <a href="/wiki/Flying_saucer" title="Flying saucer">
+          flying saucer
+        </a>
+        . The general concept of <i>Rick and Morty</i> relies on two conflicting
+        scenarios: domestic family drama and a{" "}
+        <a href="/wiki/Misanthropy" title="Misanthropy">
+          misanthropic
+        </a>{" "}
+        grandfather dragging his grandson into hijinks.
+      </p>
+      <p>
+        Roiland voiced both Rick and Morty, with{" "}
+        <a href="/wiki/Chris_Parnell" title="Chris Parnell">
+          Chris Parnell
+        </a>{" "}
+        as Jerry (Rick's son-in-law and Morty's father),{" "}
+        <a href="/wiki/Spencer_Grammer" title="Spencer Grammer">
+          Spencer Grammer
+        </a>{" "}
+        as Summer (Rick's granddaughter and Morty's sister), and{" "}
+        <a href="/wiki/Sarah_Chalke" title="Sarah Chalke">
+          Sarah Chalke
+        </a>{" "}
+        as Beth (Rick's daughter and Morty's mother). The series originated from
+        an animated{" "}
+        <a href="/wiki/Short_film" title="Short film">
+          short
+        </a>{" "}
+        <a href="/wiki/Parody_film" title="Parody film">
+          parody film
+        </a>{" "}
+        of{" "}
+        <i>
+          <a href="/wiki/Back_to_the_Future" title="Back to the Future">
+            Back to the Future
+          </a>
+        </i>{" "}
+        created by Roiland for{" "}
+        <a href="/wiki/Channel_101" title="Channel 101">
+          Channel&nbsp;101
+        </a>
+        , a short-film festival cofounded by Harmon. Since its debut, the series
+        has received critical acclaim for its screenplay, originality,
+        creativity, and humor. It has been nominated for three{" "}
+        <a
+          href="/wiki/Primetime_Emmy_Award_for_Outstanding_Animated_Program"
+          title="Primetime Emmy Award for Outstanding Animated Program"
+        >
+          Primetime Emmy Awards for Outstanding Animated Program
+        </a>{" "}
+        and won the award in 2018 and 2020. The series has also received two{" "}
+        <a href="/wiki/Annie_Awards" title="Annie Awards">
+          Annie Awards
+        </a>
+        . At times, the series has been the most viewed television comedy for
+        adults between 18 and 24. The popularity of <i>Rick and Morty</i> has
+        made it a hundred-million dollar{" "}
+        <a
+          href="/wiki/Rick_and_Morty_(franchise)"
+          title="Rick and Morty (franchise)"
+        >
+          merchandising and media franchise
+        </a>
+        .
+      </p>
+      <p>
+        A{" "}
+        <a href="/wiki/Rick_and_Morty_season_7" title="Rick and Morty season 7">
+          seventh season
+        </a>{" "}
+        was confirmed as part of a long-term deal with Cartoon Network that
+        ordered 70 new episodes, which renewed the series through to a tenth
+        season. Adult Swim cut ties with Roiland in 2023 amid allegations of
+        domestic abuse and recast his roles with soundalike actors in season 7,
+        which premiered on October 15, 2023.
+      </p>
+      <h3 className="my-3">Premise and main characters</h3>
+      <div role="note" className="hatnote navigation-not-searchable">
+        See also:{" "}
+        <a
+          href="/wiki/List_of_Rick_and_Morty_characters"
+          title="List of Rick and Morty characters"
+        >
+          List of <i>Rick and Morty</i> characters
+        </a>
+      </div>
+      <p>
+        The show revolves around the adventures of the members of the Smith
+        household, which consists of parents{" "}
+        <a
+          href="/wiki/Jerry_Smith_(Rick_and_Morty)"
+          title="Jerry Smith (Rick and Morty)"
+        >
+          Jerry
+        </a>{" "}
+        and{" "}
+        <a
+          href="/wiki/List_of_Rick_and_Morty_characters#Beth_Smith"
+          title="List of Rick and Morty characters"
+        >
+          Beth
+        </a>
+        , their children{" "}
+        <a
+          href="/wiki/List_of_Rick_and_Morty_characters#Summer_Smith"
+          title="List of Rick and Morty characters"
+        >
+          Summer
+        </a>{" "}
+        and{" "}
+        <a href="/wiki/Morty_Smith" title="Morty Smith">
+          Morty
+        </a>
+        , and Beth's father,{" "}
+        <a href="/wiki/Rick_Sanchez" title="Rick Sanchez">
+          Rick Sanchez
+        </a>
+        , who lives with them as a guest. The family lives in a suburb in the{" "}
+        <a
+          href="/wiki/American_Midwest"
+          className="mw-redirect"
+          title="American Midwest"
+        >
+          American Midwest
+        </a>
+        .
+        <sup id="cite_ref-3" className="reference">
+          <a href="#cite_note-3">[3]</a>
+        </sup>{" "}
+        The adventures of <i>Rick and Morty</i>, however, take place across an
+        infinite number of realities, with the characters traveling to other
+        planets and dimensions through portals and Rick's flying saucer.
+        <sup id="cite_ref-4" className="reference">
+          <a href="#cite_note-4">[4]</a>
+        </sup>
+      </p>
+      <p>
+        Rick is an eccentric and alcoholic{" "}
+        <a href="/wiki/Mad_scientist" title="Mad scientist">
+          mad scientist
+        </a>
+        , who eschews many ordinary conventions such as school, marriage, love,
+        and family. He frequently goes on adventures with his 14-year-old
+        grandson, Morty, a kind-hearted but easily distressed boy, whose naïve
+        but grounded moral compass plays counterpoint to Rick's{" "}
+        <a
+          href="/wiki/Machiavellianism_(psychology)"
+          title="Machiavellianism (psychology)"
+        >
+          Machiavellian ego
+        </a>
+        .
+        <sup id="cite_ref-5" className="reference">
+          <a href="#cite_note-5">[5]</a>
+        </sup>
+        <sup id="cite_ref-6" className="reference">
+          <a href="#cite_note-6">[6]</a>
+        </sup>{" "}
+        Morty's 17-year-old sister, Summer, is a more conventional teenager who
+        worries about improving her status among her peers and sometimes follows
+        Rick and Morty on their adventures.
+        <sup id="cite_ref-7" className="reference">
+          <a href="#cite_note-7">[7]</a>
+        </sup>{" "}
+        The kids' mother, Beth, is a generally level-headed person and assertive
+        force in the household, though self-conscious about her professional
+        role as a horse surgeon. She is dissatisfied with her marriage to Jerry,
+        a simple-minded and insecure person, who disapproves of Rick's influence
+        over his family.
+        <sup id="cite_ref-8" className="reference">
+          <a href="#cite_note-8">[8]</a>
+        </sup>
+      </p>
+      <p>
+        Different versions of the characters inhabit other dimensions throughout
+        the show's{" "}
+        <a
+          href="/wiki/Parallel_universes_in_fiction"
+          title="Parallel universes in fiction"
+        >
+          multiverse
+        </a>{" "}
+        and their personal characteristics can vary from one reality to another.
+        <sup id="cite_ref-9" className="reference">
+          <a href="#cite_note-9">[9]</a>
+        </sup>{" "}
+        The show's original Rick identifies himself as "Rick Sanchez of Earth
+        Dimension C-137", in reference to his original universe, but this does
+        not apply to any other member of the Smith household.
+        <sup id="cite_ref-10" className="reference">
+          <a href="#cite_note-10">[10]</a>
+        </sup>{" "}
+        For instance, in the first-season episode "
+        <a href="/wiki/Rick_Potion_No._9" title="Rick Potion No. 9">
+          Rick Potion #9
+        </a>
+        ", after turning the entire world population into monsters, Rick and
+        Morty move to a different dimension, leaving the original Summer, Beth
+        and Jerry behind.
+        <sup id="cite_ref-11" className="reference">
+          <a href="#cite_note-11">[11]</a>
+        </sup>
+      </p>
+    </div>
+  );
 }
